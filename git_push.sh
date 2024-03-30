9c1913a734742d5ea04ff80e83481319b4526264
qianlun502 <8378793+qianlun502@user.noreply.gitee.com>
2024-03-30 21:50:52 +0800

    update 2024年03月30日 21:50:52 ip_result

--- git_push.sh
+++ git_push.sh
@@ -1,11 +1,18 @@
-echo "执行的文件名：$0";
-echo "上传IP和result"
-str="/c/software/v2rayN/CloudflareST_windows_amd64/CF-WORKERS"
-pwd_str=$(pwd)
-echo "$pwd_str"
-# if [ "$pwd_str" -ne "$str" ]; then `cd "/c/software/v2rayN/CloudflareST_windows_amd64/CF-WORKERS"`; fi
-cd "/c/software/v2rayN/CloudflareST_windows_amd64/CF-WORKERS"
-echo $(pwd)
+# echo "执行的文件名：$0"
+# echo "上传IP和result"
+# str="/c/software/v2rayN/CloudflareST_windows_amd64/CF-WORKERS"
+# pwd_str=$(pwd)
+# echo "$pwd_str"
+# # if [ "$pwd_str" -ne "$str" ]; then `cd "/c/software/v2rayN/CloudflareST_windows_amd64/CF-WORKERS"`; fi
+# cd "/c/software/v2rayN/CloudflareST_windows_amd64/CF-WORKERS"
+# echo $(pwd)
+# git add .
+# git commit -m "update $(date) ip_result"
+# git push --set-upstream origin master
+
+str1="/c/software/v2rayN/CloudflareST_windows_amd64/CF-WORKERS/ip-worker-backup"
+eval $str1
+cat ./../ip.txt &> ./ip.txt
 git add .
 git commit -m "update $(date) ip_result"
 git push --set-upstream origin master
