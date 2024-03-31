a72d6d1a970a379885e9a9903ff80f49427ffe1b
qianlun502 <8378793+qianlun502@user.noreply.gitee.com>
2024-04-01 00:51:35 +0800

    update 2024年04月 1日  0:51:35 ip_result

--- test-ip-cloudspeedST-everyday.sh
+++ test-ip-cloudspeedST-everyday.sh
@@ -65,8 +65,8 @@ function test_speed() {
   done
 
 }
-file="https://www.learnwebs.top/file/movie_max.mp4"
-
+# file="https://www.learnwebs.top/file/movie_max.mp4"
+file="https://gh-proxy.learnwebs.top/https://github.com/AaronFeng753/Waifu2x-Extension-GUI/releases/download/v2.21.12/Waifu2x-Extension-GUI-v2.21.12-Portable.7z"
 rm "C:/Users/duoduo/Desktop/result1.log"
 echo 当前速度小于5的节点会被删除
 #参数设置ip测速下限
@@ -82,11 +82,12 @@ if [ "$count_line" -gt 50 ]; then
                                 echo "节点数量够多了，不需要再筛选了" &>> "C:/Users/duoduo/Desktop/result.log"
                                                                                 exit 1
 fi
-echo "$(date) insert ip start " &>> "C:/Users/duoduo/Desktop/result.log"
-cd ..
-echo pwd
-# ./../all-port-node.sh
-./all-port-node.sh $file
-echo "$(date) insert ip end " &>> "C:/Users/duoduo/Desktop/result.log"
+ echo "节点数量bbu" &>> "C:/Users/duoduo/Desktop/result.log"
+# echo "$(date) insert ip start " &>> "C:/Users/duoduo/Desktop/result.log"
+# cd ..
+# echo pwd
+# # ./../all-port-node.sh
+# ./all-port-node.sh $file
+# echo "$(date) insert ip end " &>> "C:/Users/duoduo/Desktop/result.log"
 
 #设置了windows 任务计划  每周1-5，10:30 PM 执行；每周6-7 18:30 AM 执行。
