231d6810863eaf15d5d110fb8e11432a505bd05e
qianlun502 <8378793+qianlun502@user.noreply.gitee.com>
2024-04-01 18:21:29 +0800

    update 2024年04月 1日 18:21:29 ip_result

--- test-ip-cloudspeedST-everyday.sh
+++ test-ip-cloudspeedST-everyday.sh
@@ -65,13 +65,122 @@ function test_speed() {
   done
 
 }
+#使用getopt参数解析
+function getopts_params() {
+  file=2
+  echo $@
+  ARGS=$(getopt -o  p::u::sl::tlr::tl --long port::,url::,sl::,tlr::,tl:: -- $@)
+  #  echo $ARGS
+  if [ $? != 0 ]; then
+                      echo error
+                                 exit 1
+  fi
+  echo $ARGS
+  eval set -- $ARGS
+  while [ true ]; do
+    case "${1}" in
+      -p | --port)
+        echo "port = $2"
+        port=$2
+        shift 2
+        ;;
+      -u | --url)
+        echo "url= $2 "
+         url=$2
+        shift 2
+        ;;
+      -tlr)
+        echo "tlr= $2 "
+        shift 2
+        ;;
+      --)
+        echo end
+        break
+        ;;
+      *)
+        echo "default (none of above)"
+        exit
+        ;;
+    esac
+
+  done
+
+}
+#使用getopt参数解析
+function getopts_params2() { #直接解析，不适用getopt
+  echo $@
+  ARGS=$@
+  echo $ARGS
+  eval set -- $ARGS
+  while [ true ]; do
+    case "${1}" in
+      -tp)
+        echo "tp port = $2"
+        tp=$2
+        shift 2
+        ;;
+      -url)
+        echo "url= $2 "
+         url=$2
+        shift 2
+        ;;
+      -tls)
+        echo "tlr= $2 "
+        tls=$2
+        shift 2
+        ;;
+      -sl)
+        echo "tlr= $2 "
+        sl=$2
+        shift 2
+        ;;
+      --)
+        echo end
+        break
+        ;;
+            "")
+        echo "echo end"
+        break
+        ;;
+      *)
+        echo "default (none of above)"
+        exit 1
+        ;;
+    esac
+
+  done
+
+}
+# echo $@
+# file=1
+# getopts_params $@
+# echo $file
+# # getopts_params2 -port 443 -tls 0.4 -url 0.9
+# echo $port,$url
+# ##getopts_params -p443 -u909 -tlr343 3434 #-p '443' -u '909' -t -l 'r343' -- '3434' #-tlr 被差分为-t -l
+# ##getopts_params  --port=443 --url=909 343 3434
+
+#使用getopt参数解析
+echo $0
+getopts_params2 $@
 # file="https://www.learnwebs.top/file/movie_max.mp4"
 file="https://gh-proxy.learnwebs.top/https://github.com/AaronFeng753/Waifu2x-Extension-GUI/releases/download/v2.21.12/Waifu2x-Extension-GUI-v2.21.12-Portable.7z"
+file1="${url:-$file}"
+echo "$file1"
+file=$file1
+echo "$file"
+
+sl=${sl:-5} #若未设置速度下限，默认为5
+tls=${tls:-0.3} #丢包
+echo "\$sl:"$sl,"\$tls:"$tls,"\$url:"$file
+
+# echo exit 未注释
+# exit 0
 rm "C:/Users/duoduo/Desktop/result1.log"
 echo 当前速度小于5的节点会被删除
 #参数设置ip测速下限
 # test_speed 5 >> "./result1.log" 2>&1
-test_speed 5
+test_speed $sl
 echo "$(date) delete ip start " &>> "C:/Users/duoduo/Desktop/result.log"
 ./git_push.sh
 echo "$(date) delete ip end " &>> "C:/Users/duoduo/Desktop/result.log"
@@ -80,14 +189,13 @@ echo "$(date) delete ip end " &>> "C:/Users/duoduo/Desktop/result.log"
 count_line=$(awk 'END{print NR}' ip.txt)
 if [ "$count_line" -gt 50 ]; then
                                 echo "节点数量够多了，不需要再筛选了" &>> "C:/Users/duoduo/Desktop/result.log"
-                                                                                exit 1
 fi
- echo "节点数量bbu" &>> "C:/Users/duoduo/Desktop/result.log"
-# echo "$(date) insert ip start " &>> "C:/Users/duoduo/Desktop/result.log"
-# cd ..
-# echo pwd
-# # ./../all-port-node.sh
-# ./all-port-node.sh $file
-# echo "$(date) insert ip end " &>> "C:/Users/duoduo/Desktop/result.log"
-
-#设置了windows 任务计划  每周1-5，10:30 PM 执行；每周6-7 18:30 AM 执行。
+echo "节点数量不够,请添加节点" &>> "C:/Users/duoduo/Desktop/result.log"
+echo "$(date) insert ip start " &>> "C:/Users/duoduo/Desktop/result.log"
+cd ..
+echo pwd
+# ./../all-port-node.sh
+  #./all-port-node.sh $file
+./all-port-node.sh $@
+echo "$(date) insert ip end " &>> "C:/Users/duoduo/Desktop/result.log"
+# 设置了windows 任务计划  每周1-5，10:30 PM 执行；每周6-7 18:30 AM 执行。
