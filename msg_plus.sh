a35d3fa05829b62ca3def487e5be360e257364bd
qianlun502 <8378793+qianlun502@user.noreply.gitee.com>
2024-04-06 20:43:45 +0800

    update 2024年04月 6日 20:43:45 ip_result

--- msg_plus.sh
+++ msg_plus.sh
@@ -1,5 +1,37 @@
 echo 使用pushplus 推送消息记录
+function getopts_params2() { #直接解析，不适用getopt
+  echo $@
+  ARGS=$@
+  echo $ARGS
+  eval set -- $ARGS
+  while [ true ]; do
+    case "${1}" in
+      -referer)
+        echo "tp port = $2"
+        referer=$2
+        shift 2
+        ;;
+      --)
+        echo end
+        break
+        ;;
+            "")
+        echo "echo end"
+        break
+        ;;
+      *)
+        echo "default (none of above)"
+        exit 1
+        ;;
+    esac
+
+  done
+
+}
 # https://www.pushplus.plus/push1.html
+count_line=$(awk 'END{print NR}' ip.txt)
+echo 调用的程序名称 $@
+json_body="{\"token\":\"3259650b04724322bb80e7c3c8133402\",\"title\":\"work task success\",\"content\":\" now ip count $count_line,\"template\":\"html\",\"channel\":\"wechat\"}"
 curl 'https://www.pushplus.plus/api/send' \
   -H 'Accept: */*' \
   -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6' \
@@ -7,4 +39,4 @@ curl 'https://www.pushplus.plus/api/send' \
   -H 'Content-Type: application/json;charset=UTF-8' \
   -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0' \
   -H 'X-Requested-With: XMLHttpRequest' \
-  --data-raw '{"token":"3259650b04724322bb80e7c3c8133402","title":"work task success end ","content":" ","template":"html","channel":"wechat"}'
+  --data-raw '{"token":"3259650b04724322bb80e7c3c8133402","title":" task success end ","content":"'$referer''$count_line'","template":"html","channel":"wechat"}'
