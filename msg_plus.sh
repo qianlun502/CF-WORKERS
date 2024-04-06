cc49c3e3fdff1267244840ba1c3c6d7d33c413bf
qianlun502 <8378793+qianlun502@user.noreply.gitee.com>
2024-04-06 20:01:50 +0800

    update 2024年04月 6日 20:01:50 ip_result

---
+++ msg_plus.sh
@@ -0,0 +1,9 @@
+echo 使用pushplus 推送消息记录
+params_push="{
+\"token\": \"409ea8a9a1cc4854ad3ca04c019dcc81\",
+        \"title\": title,
+        \"content\": msg,
+        \"template\": \"html\",
+        \"channel\": \"wechat\", }"
+url_push='https://www.pushplus.plus/send'
+echo $params_push
