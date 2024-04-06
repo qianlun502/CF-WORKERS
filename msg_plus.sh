3c1b0fb3594dd24bcc2288b9feef9d994277b2f8
qianlun502 <8378793+qianlun502@user.noreply.gitee.com>
2024-04-06 20:36:49 +0800

    update 2024年04月 6日 20:36:49 ip_result

--- msg_plus.sh
+++ msg_plus.sh
@@ -1,5 +1,5 @@
 echo 使用pushplus 推送消息记录
-count_line=$(awk 'END{print NR}' ip.txt)
+# https://www.pushplus.plus/push1.html
 curl 'https://www.pushplus.plus/api/send' \
   -H 'Accept: */*' \
   -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6' \
@@ -7,5 +7,4 @@ curl 'https://www.pushplus.plus/api/send' \
   -H 'Content-Type: application/json;charset=UTF-8' \
   -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0' \
   -H 'X-Requested-With: XMLHttpRequest' \
-  -H 'pushToken: b95202863d0a47b4a4247ed75e4ab586' \
-  --data-raw 
+  --data-raw '{"token":"3259650b04724322bb80e7c3c8133402","title":"work task success end ","content":" ","template":"html","channel":"wechat"}'
