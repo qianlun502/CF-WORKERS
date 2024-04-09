2dd18f0caa4307f8e6a0a5e1a91f0566d061e226
qianlun502 <8378793+qianlun502@user.noreply.gitee.com>
2024-04-09 20:14:44 +0800

    update 2024年04月 9日 20:14:44 ip_result

---
+++ CloudST-start-everyday.bat
@@ -0,0 +1,6 @@
+"D:\Program Files\Git\bin\bash.exe" --login -i -c "echo 优选官方ip task run start"
+"D:\Program Files\Git\bin\bash.exe" --login -i -c "echo `pwd`"
+"D:\Program Files\Git\bin\bash.exe" --login -i -c  " cd C:/software/v2rayN/CloudflareST_windows_amd64/CF-WORKERS&&./starter-ip.sh&>C:/software/v2rayN/CloudflareST_windows_amd64/CF-WORKERS/result1.log"
+"D:\Program Files\Git\bin\bash.exe" --login -i -c "echo `pwd`"
+"D:\Program Files\Git\bin\bash.exe" --login -i -c "echo 优选官方ip task run end"
+pause
--- CloudST.bat
+++
@@ -1,8 +0,0 @@
-@REM "D:\Program Files\Git\bin\bash.exe" --login -i -c  ""
-cd "C:/software/v2rayN/CloudflareST_windows_amd64/CF-WORKERS"
-"D:\Program Files\Git\bin\bash.exe" --login -i -c "pwd"
-"D:\Program Files\Git\bin\bash.exe" --login -i -c  " ./test-ip-cloudspeedST-everyday.sh"
-"cd .."
-"D:\Program Files\Git\bin\bash.exe" --login -i -c  "./all-port-node.sh"
-pause
-@REM https://blog.csdn.net/themagickeyjianan/article/details/127487205 bat调用sh程序
