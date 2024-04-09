87b720415a2579478893427901d6491cd4616890
qianlun502 <8378793+qianlun502@user.noreply.gitee.com>
2024-04-09 22:56:49 +0800

    update 2024年04月 9日 22:56:49 ip_result

---
+++ test-ip-cloudspeedST-everyday.bat
@@ -0,0 +1,6 @@
+"D:\Program Files\Git\bin\bash.exe" --login -i -c "echo test-ip-cloudspeedST-everyday task run start"
+@REM "D:\Program Files\Git\bin\bash.exe" --login -i -c "echo `pwd`"
+"D:\Program Files\Git\bin\bash.exe" --login -i -c  " cd C:/software/v2rayN/CloudflareST_windows_amd64/CF-WORKERS&&./starter-ip.sh&>C:/software/v2rayN/CloudflareST_windows_amd64/CF-WORKERS/result1.log"
+@REM "D:\Program Files\Git\bin\bash.exe" --login -i -c "echo `pwd`"
+"D:\Program Files\Git\bin\bash.exe" --login -i -c "echo test-ip-cloudspeedST-everyday task run end"
+pause
