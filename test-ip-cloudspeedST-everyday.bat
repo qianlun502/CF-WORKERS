9be419dfe3dcab5d336257e7cbbdd2e0f1260169
qianlun502 <8378793+qianlun502@user.noreply.gitee.com>
2024-04-10 16:35:25 +0800

    update .gitignore

--- test-ip-cloudspeedST-everyday.bat
+++
@@ -1,6 +0,0 @@
-"D:\Program Files\Git\bin\bash.exe" --login -i -c "echo test-ip-cloudspeedST-everyday task run start"
-"D:\Program Files\Git\bin\bash.exe" --login -i -c "echo `pwd`"
-"D:\Program Files\Git\bin\bash.exe" --login -i -c  " cd C:/software/v2rayN/CloudflareST_windows_amd64/CF-WORKERS&&./starter-ip.sh&>C:/software/v2rayN/CloudflareST_windows_amd64/CF-WORKERS/result1.log"
-"D:\Program Files\Git\bin\bash.exe" --login -i -c "echo `pwd`"
-"D:\Program Files\Git\bin\bash.exe" --login -i -c "echo test-ip-cloudspeedST-everyday task run end"
-pause
